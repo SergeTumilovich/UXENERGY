ab266d3978de30ddc0611e6fc6ad8d03f3dde7f3
SergeTumilovich <30292087+SergeTumilovich@users.noreply.github.com>
2017-09-28 14:45:03 +0300

    Interface #1

--- js/script.js
+++ js/script.js
@@ -1,20 +1,80 @@
 //video play
-var overlay = document.getElementsByClassName('play');
-var vid = document.getElementById('promo-review');
+var play = document.querySelector(".play");
+var vid = document.getElementById("promo-review");
+//slider-promo
+var slider = document.querySelectorAll(".slider-promo .slider");
+var leads = document.querySelectorAll(".lead");
+//slider-mobile
+var mslide=document.querySelectorAll(".mobile-slider .slider");
+var image=document.querySelector(".image-container");
+//scroll promo between two leads
+for(var i=0;i<slider.length; i++)
+{
+	slider[i].addEventListener("click", function(evt)
+	{
+		evt.preventDefault();
+		for(var j = 0; j<leads.length; j++)
+		{
+			leads[j].classList.toggle("visually-hidden");
+		}
+	});
+}
 
-if(overlay.addEventListener){
-		overlay.addEventListener("click", play, false)
-	}else if(overlay.attachEvent){
-		overlay.attachEvent("onclick", play)
-	}
+//play event (play svg hides when video plays and shows in the other case)
+if(play.addEventListener){
+	play.addEventListener("click", play, false)
+}
+else if(play.attachEvent){
+	play.attachEvent("onclick", play);
+}
 
 //hides or shows play button when video plays or pauses
 function play() {
     if (vid.paused){
         vid.play();
-        overlay.className = "o";
+        play.classList.add("visually-hidden");
     }else {
         vid.pause();
-        overlay.className = "";
+        play.classList.remove("visually-hidden");
     }
 }
+
+//mobile-slider
+for(var i=0;i<mslide.length;i++){
+	mslide[i].addEventListener("click", function(evt, i){
+		console.log(i);
+		var curslide=mslide[i];
+		console.log(curslide);
+		evt.preventDefault();
+		/*if(!mslide.classList.contains("right")){
+			console.log("left");
+		}
+		else{
+			console.log("right");
+		}*/
+	});
+}
+
+
+/*var btn = document.querySelector("button");
+var imgs = document.querySelectorAll(".card__heading-img img");
+btn.addEventListener("click", function()
+{
+
+	for(var i = 0; i < imgs.length; i++)
+  {
+  	if (imgs[i].classList.contains("active"))
+    {
+    	imgs[i].classList.remove("active");
+      if (i < imgs.length - 1)
+      {
+      	imgs[i + 1].classList.add("active");
+      }
+      else
+      {
+      	imgs[0].classList.add("active");
+      }
+      break;
+    }
+  }
+});*/
